e5e202ab43a6dd462997b065abc0b3eb55c07997
pengchen98 <44000029+pengchen98@users.noreply.github.com>
2020-04-14 13:29:49 -0500

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,13 +2,19 @@ data "aws_caller_identity" "current" {}
 
 data "aws_ami" "ec2_instance" {
   most_recent = true
+  
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
 
-  owners = ["${data.aws_caller_identity.current.account_id}"]
+  owners = ["752717405135"]
 }
 
 resource "aws_instance" "web" {
